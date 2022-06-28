be2d5bb1576dc41725517cee4ac02e5e61ebf387
Suryanshu <suryanshushukla2001@gmail.com>
2022-06-28 14:29:18 +0530

    This is for only checking

--- app/src/main/java/com/example/bottomnavigation/activities/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/activities/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewpager.adapter = ViewpagetAdapter(supportFragmentManager)
+        //        only for checking git
     }
 
 }
